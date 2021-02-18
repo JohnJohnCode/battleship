30e9fac7661941713308e5a8f9b0de675e6b665e
JohnJohnCode <jsynthlips@gmail.com>
2021-02-18 17:13:41 +0100

    Updates

---
+++ precache-manifest.fc8a0ee5102528901f04b48f3e5d64e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec14a4fa613b7bae695fc59d7de827c",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "999bd92a101c7da04023",
+    "url": "/battleship/static/css/main.7b863fac.chunk.css"
+  },
+  {
+    "revision": "f0280c29451b34b089c0",
+    "url": "/battleship/static/js/2.402ff0f9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/battleship/static/js/2.402ff0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999bd92a101c7da04023",
+    "url": "/battleship/static/js/main.8f3cc703.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
